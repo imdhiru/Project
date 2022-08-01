a98d234a3cc0e3cfd68007ab1ad9e9346ab1b599
Sudhir kumar <swetakumari.430@gmail.com>
2022-08-01 10:11:32 +0530

    Update App.js
    Update date issue

--- src/App.js
+++ src/App.js
@@ -5,36 +5,29 @@ import { postMedication } from "./Services/Api";
 import './App.css';
 
 
-
-
 const patientData = {
   name: "John Smith",
   dob: "May 9, 1993",
   age: "25 Years",
   gender: "Male"
 }
-const medicationName = ["Asprin", "Amoxicillin", "Ibuprofen", "Azithromycin"];
+const medicationName = ["Asprin", "Azithromycin","Amoxicillin","Asthalin","Augmentin" ,"Ibuprofen","","Paracetamol","Pantop"];
 
 function App() {
-  
+  // Define Intial State to add empty input row
   const [inputFields, setInputFields] = React.useState([{ "name": "", "strength": "", "frequency": "", "dosageForm": "", "route": "", "duration": "", "startDateTime": "", "endDateTime": "", "timeOfMedication": "", "additionalComments": "" }]);
-
-
-  
+  //Define onchange event for input fields
   const onChange = onChanges(inputFields, setInputFields);
-  
-  
-
+  //Submit Input Form function 
   const submit = (e) => {
     e.preventDefault();
-    //const postAPIData = DataPrep(inputFields);
-    PostDataCall(DataPrep(inputFields),postMedication);
+    PostDataCall(DataPrep(inputFields), postMedication);
   }
   return (
     <div className="App">
       <UserInfo details={patientData} />
       <form onSubmit={submit}>
-        <PrescriptionTable medicationName={medicationName} data={inputFields} add={addNewRow(setInputFields, inputFields)} change={onChange}/>
+        <PrescriptionTable medicationName={medicationName} data={inputFields} add={addNewRow(setInputFields, inputFields)} change={onChange} />
         <button className="btn btn-primary ml-3" data-testid={"submit-test"} onClick={submit}>Submit</button>
       </form>
     </div>
@@ -43,17 +36,16 @@ function App() {
 
 export default App;
 
-
-export function PostDataCall(postAPIData,postMedication) {
+// Post API call method
+export function PostDataCall(postAPIData, postMedication) {
   postMedication(postAPIData).then(response => {
     alert("Prescription Updated Successfully", response);
-    return true;
   })
     .catch(err => {
-    alert(err.message);
+      alert(err.message);
     });
 }
-
+//Update and set input fields value in state
 export function onChanges(inputFields, setInputFields) {
   return (e, index) => {
     let updateData = [...inputFields];
@@ -62,16 +54,25 @@ export function onChanges(inputFields, setInputFields) {
     return true;
   };
 }
-
+// add Patient ID and Date formatupdate for  API data  
 export function DataPrep(inputFields) {
   return inputFields.map((data) => {
-    data.startDateTime = data.startDateTime.split('T')[0] + " " + data.startDateTime.split('T')[1] + ":00";
-    data.endDateTime = data.endDateTime.split("T")[0] + " " + data.endDateTime.split('T')[1] + ":00";
+    data.startDateTime = DateTimeFormater(data.startDateTime);
+    data.endDateTime = DateTimeFormater(data.endDateTime);
     data.patientId = "0";
     return data;
   });
 }
-
+//Date format handle
+export function DateTimeFormater(data) {
+  if (data.includes('T')) {
+    return data.split('T')[0] + " " + data.split('T')[1] + ":00";
+  }
+  else {
+    return data;
+  }
+}
+// Add new Row for input
 export function addNewRow(setInputFields, inputFields) {
   let inputFieldsdata = { "name": "", "strength": "", "frequency": "", "dosageForm": "", "route": "", "duration": "", "startDateTime": "", "endDateTime": "", "timeOfMedication": "", "additionalComments": "" };
   return () => {
