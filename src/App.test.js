5f6c271d32fa4b64d570e466e9b8826a12d9b766
Sudhir kumar <swetakumari.430@gmail.com>
2022-08-01 10:12:25 +0530

    Update App.test.js
    added test cases

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
 import { useState } from 'react';
 import axios from 'axios';
-import App,{addNewRow, DataPrep, onChanges, PostDataCall} from './App';
+import App,{addNewRow, DataPrep, onChanges, PostDataCall, DateTimeFormater} from './App';
 import { postMedication} from './Services/Api';
 
 test('renders App component', () => {
@@ -64,5 +64,38 @@ test('Test Add new row function with State update', async () => {
  expect(jest.fn()).toHaveBeenCalledTimes(0);
 });
 
+//  test('Test Post API call method', async () => {
+// //   const setInputFields =jest.fn();
+// const data1={message:"done"};
+//    //const postMedications = jest.fn().mockReturnValue(() => Promise.resolve(data1));
+//   const postMedications = jest.fn(() => {return Promise.resolve()});
 
+//   //  axios.post.mockResolvedValue(() =>
+//   //   Promise.resolve({
+//   //     data: { message:"done" }
+//   //   })
+//   // );
+// //   const response = true;
+// //   //const inputFields : (useState)=>[useState,setInputFields];
+// //   //xios.post.mockResolvedValueOnce(response);
+// //   //const response = { json: jest.fn().mockResolvedValueOnce({message:"Updated Successfully"}) };
+//    let inputFieldsdata = { "name": "a", "strength": "t", "frequency": "", "dosageForm": "", "route": "", "duration": "", "startDateTime": "2022-07-29T17:16", "endDateTime": "2022-07-29T17:16", "timeOfMedication": "", "additionalComments": "" };
+//    //const data = await PostDataCall([inputFieldsdata],postMedications);
+
+//    //void PostDataCall([inputFieldsdata],postMedications).then(() => {
+//     expect(functionBSpy).toHaveBeenCalledTimes(1);
+//   });
+//    await expect(PostDataCall([inputFieldsdata],postMedications)).resolves.toBe('peanut butter');
+//    expect(data).resolves.toBeUndefined();
+// //expect(postMedication).toHaveBeenCalledTimes(1);
+//  });
+
+test('Test Date time Formatter function', async () => {
+  const Date = DateTimeFormater("2022-08-05T19:10");
+ expect(Date).toBe("2022-08-05 19:10:00");
+});
+test('Test Date time Formatter function without T format', async () => {
+  const Date = DateTimeFormater("2022-08-05 19:10");
+ expect(Date).toBe("2022-08-05 19:10");
+});
 
